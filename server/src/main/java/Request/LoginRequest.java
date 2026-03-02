793710a4f3583bb0c5dc065783c472cc474e1719
tjhinck <tjhinck@byu.edu>
2026-03-02 00:26:53 -0700

    Login and register unit tests passing

--- server/src/main/java/Request/LoginRequest.java
+++ server/src/main/java/Request/LoginRequest.java
@@ -1,6 +1,12 @@
 package Request;
 
+import java.util.Objects;
+
 public record LoginRequest(
         String username,
         String password) {
+    public LoginRequest{
+        Objects.requireNonNull(username, "username required");
+        Objects.requireNonNull(password, "password required");
+    }
 }
--- server/src/main/java/Request/RegisterRequest.java
+++ server/src/main/java/Request/RegisterRequest.java
@@ -1,7 +1,14 @@
 package Request;
 
+import java.util.Objects;
+
 public record RegisterRequest(
         String username,
         String password,
         String email) {
+    public RegisterRequest{
+        Objects.requireNonNull(username, "username required");
+        Objects.requireNonNull(password, "password required");
+        Objects.requireNonNull(email, "email required");
+    }
 }
