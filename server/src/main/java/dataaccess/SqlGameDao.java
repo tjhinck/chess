86e31918ee110ef9ae9ed02b90873cb37aeebab2
tjhinck <tjhinck@byu.edu>
2026-03-11 14:20:06 -0600

    ready to mess up gameData

--- server/src/main/java/dataaccess/SqlGameDao.java
+++ server/src/main/java/dataaccess/SqlGameDao.java
@@ -1,18 +1,46 @@
 package dataaccess;
 
+import model.AuthData;
 import model.GameData;
+import server.Server;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
 public class SqlGameDao implements GameDao{
+
+    public SqlGameDao() throws DataAccessException {
+        configureDatabase();
+    }
+
     @Override
     public void addGame(GameData gameData) throws DataAccessException {
-
+        var statement = "INSERT INTO games (gameID, gameDataJson) VALUES (?, ?)";
+        String json = Server.GSON.toJson(gameData);
+        executeUpdate(statement, gameData.getGameID(), json);
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT gameDataJson FROM games WHERE gameID=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return Server.GSON.fromJson(rs.getString("gameDataJson"), GameData.class);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("unable to get data: %s", e.getMessage()));
+        }
         return null;
     }
 
@@ -23,6 +51,52 @@ public class SqlGameDao implements GameDao{
 
     @Override
     public void clearData() throws DataAccessException {
+        var statement = "TRUNCATE games";
+        executeUpdate(statement);
+    }
 
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof GameData p) ps.setString(i + 1, p.toString());
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  games (
+              gameID int NOT NULL PRIMARY KEY,
+              gameDataJson JSON NOT NULL,
+              INDEX(gameID)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
