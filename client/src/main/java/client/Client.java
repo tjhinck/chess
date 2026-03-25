fd4bf93708b84133aa13026eaf1f4b8e1f417058
tjhinck <tjhinck@byu.edu>
2026-03-24 23:22:52 -0600

    basic structure for client loop

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,4 +1,81 @@
 package client;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+import exception.ResponseException;
+import server.ServerFacade;
+
+import static ui.EscapeSequences.*;
+
 public class Client {
+    private final ServerFacade server;
+    private State state = State.SIGNED_OUT;
+
+    private enum State {
+        SIGNED_IN,
+        SIGNED_OUT
+    }
+
+    public Client(String serverURL){
+        server = new ServerFacade(serverURL);
+    }
+
+    public void run(){
+        System.out.println("Welcome to chess. Please sign in to begin");
+        System.out.print(help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+            try {
+                result = eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println("Goodbye!");
+        System.out.println();
+    }
+
+    private String eval(String input){
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "signin" -> signIn(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    private String signIn(String... params){
+
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR  + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+
+    private String help(){
+        if (state == State.SIGNED_OUT){
+            return """
+                    - signIn <username>
+                    - quit
+                    """;
+        }
+        return """
+                - list
+                - quit
+                """;
+    }
 }
--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -4,12 +4,25 @@ import chess.*;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
-        var board = new ChessBoard();
-        var copy = board.clone();
-        board.resetBoard();
-        System.out.println(board);
-        System.out.println(copy);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Client: " + piece);
+//        var board = new ChessBoard();
+//        var copy = board.clone();
+//        board.resetBoard();
+//        System.out.println(board);
+//        System.out.println(copy);
+
+        String serverUrl;
+        if (args.length > 1) {
+            serverUrl = args[0];
+        } else {
+            serverUrl = "http://localhost:8080";
+        }
+        try {
+            new Client(serverUrl).run();
+
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
     }
 }
